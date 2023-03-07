9949f535f8b6d53aade91918474c5ddd545ac9d7
Niteesh Kumar <pinjalaniteesh@gmail.com>
2023-03-07 10:40:13 +0000

    Cloud watch logs set up

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,6 +22,20 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+#----------------------CloudWatch-------------------------------
+
+import watchtower
+import logging
+from time import strftime
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
+
 #---------------------------X-Ray---------------------------------
 
 from aws_xray_sdk.core import xray_recorder
@@ -69,6 +83,12 @@ def data_message_groups():
   else:
     return model['data'], 200
 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
   user_sender_handle = 'andrewbrown'
@@ -97,7 +117,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])

80800b83ee436fe70ab5aff2f8ae4d32b9557d7d
Niteesh Kumar <pinjalaniteesh@gmail.com>
2023-03-10 09:42:19 +0000

    Rollbar Configuration

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,6 +36,12 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 # LOGGER.addHandler(cw_handler)
 # LOGGER.info("test log")
 
+#-------------------------Rollbar-----------------------------
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 #---------------------------X-Ray---------------------------------
 
 # from aws_xray_sdk.core import xray_recorder
@@ -54,6 +60,25 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+
+#-------------------------Rollbar---------------------------------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 #---------------------------X-Ray--------------------------------
 # XRayMiddleware(app, xray_recorder)
@@ -89,6 +114,13 @@ def data_message_groups():
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
+
+#-------------------------Rollbar--------------------------------------
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
